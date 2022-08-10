bd25135f40092b65dd9aaa469c32b1acb0d89426
elena <lena.kazakova99@gmail.com>
2022-08-11 00:40:25 +0400

    Задача 2

--- 1_lesson/1_2/Program.cs
+++ 1_lesson/1_2/Program.cs
@@ -2,38 +2,38 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.Write("Введите номер дня недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
-
-if (day == 1)
+Console.Write("Введите целое число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string day = "";
+if (num >= 1 && num <= 7)
 {
-    Console.WriteLine("Понедельник");
-}
-else if (day == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if (day == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (day == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (day == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if (day == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (day == 7)
-{
-    Console.WriteLine("Воскресенье");
+    switch (num)
+    {
+        case 1:
+            day = "Понедельник";
+            break;
+        case 2:
+            day = "Вторник";
+            break;
+        case 3:
+            day = "Среда";
+            break;
+        case 4:
+            day = "Четверг";
+            break;
+        case 5:
+            day = "Пятница";
+            break;
+        case 6:
+            day = "Суббота";
+            break;
+        case 7:
+            day = "Воскресенье";
+            break;
+    }
+    Console.WriteLine("День недели: " + day);
 }
 else
 {
-    Console.WriteLine("Такого дня не существует");
+    Console.WriteLine("Вы ввели некорректное число");
 }
