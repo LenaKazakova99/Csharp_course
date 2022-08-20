7ea55d8ee382db2a6d13c9c1e789abd4e4c89c99
elena <lena.kazakova99@gmail.com>
2022-08-20 15:10:28 +0400

    Задача 12. По счету 3. Переделала с методом

--- 2_lesson/2_3/Program.cs
+++ 2_lesson/2_3/Program.cs
@@ -9,11 +9,16 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 % num2 == 0)
+void CheckNum(int num1, int num2)
 {
-    Console.WriteLine ($"Число {num1} кратно числу {num2}");
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine($"Число {num1} кратно числу {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num1} не кратно числу {num2}. Остаток {num1 % num2}");
+    }
 }
-else
-{
-    Console.WriteLine($"Число {num1} не кратно числу {num2}. Остаток {num1 % num2}");
-}
+
+CheckNum(num1, num2);
