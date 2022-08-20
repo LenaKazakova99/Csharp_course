0f0292472d214bbf527df34cc74d452dbf44cd95
elena <lena.kazakova99@gmail.com>
2022-08-20 14:34:24 +0400

    Задача 11. По счету 2

---
+++ 2_lesson/2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 11. Напишите программу, которая выводит случайное трехзначное число и удаляет
+//     вторую цифру этого числа.
+//     456 -> 46
+//     782 -> 72
+//     918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int deleteMiddleDigit(int num)
+{
+    int second = number / 100;
+    int first = number % 10;
+    int result = second * 10 + first;
+    return result;
+}
+
+int res = deleteMiddleDigit(number);
+Console.WriteLine($"Сгенерированное число {number}. Обработанное число -> {res}");
