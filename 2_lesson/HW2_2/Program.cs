e82504eccf99f717e64bf1917f01c55ae659e61e
elena <lena.kazakova99@gmail.com>
2022-08-20 22:37:23 +0400

    HW Задача 13

---
+++ 2_lesson/HW2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//            что третьей цифры нет. 
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ThirdDigit(int num)
+{
+    if (num < 99)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return;
+    }
+    while (num > 999) num = num / 10;
+    Console.WriteLine(num % 10);
+}
+ThirdDigit(num);
