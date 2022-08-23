08e911987b3ed8940c70ee40e3d2f007ea15de27
elena <lena.kazakova99@gmail.com>
2022-08-23 21:10:04 +0400

    HW задача 23

---
+++ 3_lesson/HW3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//            3 -> 1, 8, 27 
+//            5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Kub(int N)
+{
+    int count = 1;
+    while (count <= N)
+    {
+        Console.WriteLine($"{count } | {Math.Pow(count, 3)}");
+        count++;
+    }
+}
+
+Kub(N);
