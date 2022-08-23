8e858cb3856e211f943deae5828a277456f4d527
elena <lena.kazakova99@gmail.com>
2022-08-24 00:28:31 +0400

    задача 26, по счету 2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдает количество
+//            цифр в числе.
+//            456 -> 3
+//            78 -> 2
+//            89126 -> 5
+
+Console.WriteLine("Введите целое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int CountNumbers(int num)
+{
+    int counter = 1;
+    while (A >= 10)
+    {
+        A = A / 10;
+        counter++;
+    }
+    return counter;
+}
+int result = CountNumbers(A);
+
+Console.WriteLine($"Количество цифр в числе -> {result}");
+
+// int counter = 1;
+// for (int i = 1; A >= 10; A = A / 10)
+// {
+//     counter++;
+// }
+
+// Console.WriteLine($"Количество цифр в числе -> {counter}");
