438c95b2044e26dc1fe72e35b31efa1a0efc61fa
elena <lena.kazakova99@gmail.com>
2022-08-28 15:19:56 +0400

    задача 28, по счету 3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N и выдает
+//            произведение чисел от 1 до N.
+//            4 -> 24
+//            5 -> 120
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int MultNum(int num)
+{
+    int m = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        // sum = sum + 1;
+        m *= i;
+    }
+    return m;
+}
+
+int mult = MultNum(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} равно: " + mult);
+
