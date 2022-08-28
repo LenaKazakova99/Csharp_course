c67d88d044e5120465d0ae9d210f8fb0644b77d9
elena <lena.kazakova99@gmail.com>
2022-08-28 15:35:42 +0400

    задача 30, по счету 4

---
+++ 4_lesson/4_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 30. Напишите программу, которая выводит массив из 8 элементов, 
+//            заполненный нулями и единицами в случайном порядке.
+//            [1,0,1,1,0,1,0,0]
+
+int[] result = FillArray(8, 0, 2);
+PrintArr(result);
+
+int[] FillArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
