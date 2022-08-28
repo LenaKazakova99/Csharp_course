3d31b2432d1e4f862b82526c55fc14751617c5cd
elena <lena.kazakova99@gmail.com>
2022-08-28 15:50:24 +0400

    задача 30, по счету 4

--- 4_lesson/4_4/Program.cs
+++ 4_lesson/4_4/Program.cs
@@ -15,10 +15,23 @@ int[] CreateArray(int n, int min, int max)
     return array;
 }
 
+// void PrintArr(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == 0) Console.Write("[");
+//         if (i < array.Length - 1) Console.Write(array[i] + ",");
+//         else Console.Write(array[i] + "]");
+//     }
+// }
+
 void PrintArr(int[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
     }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
 }
