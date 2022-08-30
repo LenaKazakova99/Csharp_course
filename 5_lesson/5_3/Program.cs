f3441c0df64596c9ab1dc92ff8c3a8639b86936c
elena <lena.kazakova99@gmail.com>
2022-08-30 18:45:50 +0400

    задача 33, 5 семинар

---
+++ 5_lesson/5_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+//            определяет, присутствует ли заданное число в массиве.
+//            4; массив [6, 7, 19, 345, 3] -> нет
+//            3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+bool TrueFalse(int[] array, int elem)
+{
+    bool ret = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == elem)
+        {
+            ret = true;
+            break;
+        }
+    }
+    return ret;
+}
+
+Console.WriteLine("Введите число: ");
+int findI = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(12, -9, 9);
+PrintArray(arr);
+bool search = TrueFalse(arr, findI);
+Console.WriteLine();
+Console.WriteLine(search ? $"Число {findI} присутствует в массиве" : $"Число {findI} отсутствует в массиве");
