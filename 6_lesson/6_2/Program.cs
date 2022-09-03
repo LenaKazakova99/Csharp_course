f36e4a37f70ce6ddce2db5eb7b5e9515433210be
elena <lena.kazakova99@gmail.com>
2022-09-03 14:26:22 +0400

    задача 40, 6 семинар

--- 6_lesson/6_2/Program.cs
+++ 6_lesson/6_2/Program.cs
@@ -3,3 +3,23 @@
 //            такой длины.
 //            Теорема о неравенстве треугольника: каждая сторона треугольника
 //            меньше суммы двух других сторон.
+
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+bool CheckTriangle(int a, int b, int c)
+{
+    //     if (a1 + b1 > c1 && b1 + c1 > a1 && a1 + c1 > b1)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    return a + b > c && b + c > a && a + c > b;
+}
+bool result = CheckTriangle(a, b, c);
+// if (result) Console.WriteLine("Три числа являются сторонами треугольника");
+// else Console.WriteLine("Три числа не являются сторонами треугольника");
+Console.Write(result ? "Три числа являются сторонами треугольника" : "Три числа являются сторонами треугольника");
